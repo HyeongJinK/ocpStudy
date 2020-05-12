63fd3657f405a5604db76bff156c7063cbb17656
napasun <wkzkfmxk23@gmail.com>
2020-05-12 19:00:16 +0900

    문자열 반전

--- java_answer/Reversing_letters_and_words/src/main/java/khj/study/App.java
+++ java_answer/Reversing_letters_and_words/src/main/java/khj/study/App.java
@@ -1,13 +1,49 @@
 package khj.study;
 
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 /**
- * Hello world!
- *
+ * 단어 반전
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( reverseWords("Hello World!"));
+        System.out.println( reverseWords2("Hello World!"));
+        System.out.println( reverse("Hello World!"));
+    }
+
+    private static final String WHITESPACE = " ";
+
+    public static String reverseWords(String str) {
+        String[] words = str.split(WHITESPACE);     //공백으로 단어 구
+        StringBuilder reversedString = new StringBuilder();
+
+        for (String word: words) {
+            StringBuilder reverseWord = new StringBuilder();
+
+            for (int i = word.length() - 1; i >= 0; i--) {  // 역순으로 가져와서 단어를 뒤집는다.
+                reverseWord.append(word.charAt(i));
+            }
+
+            reversedString.append(reverseWord).append(WHITESPACE);
+        }
+
+        return reversedString.toString();
+    }
+
+    private static final Pattern PATTERN = Pattern.compile(" +");
+
+
+    public static String reverseWords2(String str) {
+        return PATTERN.splitAsStream(str)
+                .map(w -> new StringBuilder(w).reverse())
+                .collect(Collectors.joining(" "));
+    }
+
+    public static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 }
