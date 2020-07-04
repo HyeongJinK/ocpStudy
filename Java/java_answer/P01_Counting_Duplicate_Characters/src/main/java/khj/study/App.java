6977fb6fa923d495202bdf97c32d1431b0e02faf
napasun <wkzkfmxk23@gmail.com>
2020-07-05 02:48:22 +0900

    중복 수 가져오기 수정

--- Java/java_answer/P01_Counting_Duplicate_Characters/src/main/java/khj/study/App.java
+++ Java/java_answer/P01_Counting_Duplicate_Characters/src/main/java/khj/study/App.java
@@ -1,8 +1,10 @@
 package khj.study;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -11,10 +13,66 @@ import java.util.stream.Collectors;
  */
 public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println(countDuplicateCharactersV1("Hello World!"));
-        System.out.println(countDuplicateCharactersV2("Hello World!"));
+    private static final String TEXT = "Be strong, be fearless, be beautiful. "
+            + "And believe that anything is possible when you have the right "
+            + "people there to support you. ";
+    // Ӝ -> Unicode: \u04DC, Code Point: 1244
+    // 💕 -> Unicode: \uD83D\uDC95, Code Point: 128149
+    // 🎼 -> \uD83C\uDFBC, Code Point: 127932
+    // 😍 ->\uD83D\uDE0D, Code Point: 128525
+    private static final String TEXT_CP = TEXT + "😍 I love 💕 you Ӝ so much 💕 😍 🎼🎼🎼!";
+
+    public static void main(String[] args) {
+
+        System.out.println("Input text: \n" + TEXT + "\n");
+
+        System.out.println("\n\nASCII or 16 bits Unicode characters (less than 65,535 (0xFFFF)) examples:\n");
+
+        System.out.println("HashMap based solution:");
+        long startTimeV1 = System.nanoTime();
+
+        Map<Character, Integer> duplicatesV1 = countDuplicateCharactersV1(TEXT);
+
+        displayExecutionTime(System.nanoTime()-startTimeV1);
+        System.out.println(Arrays.toString(duplicatesV1.entrySet().toArray()));
+        // or: duplicatesV1.forEach( (k, v) -> System.out.print(k + "="+ v + ", "));
+
+        System.out.println();
+        System.out.println("Java 8, functional-style solution:");
+        long startTimeV2 = System.nanoTime();
+
+        Map<Character, Long> duplicatesV2 = countDuplicateCharactersV2(TEXT);
+
+        displayExecutionTime(System.nanoTime()-startTimeV2);
+        System.out.println(Arrays.toString(duplicatesV2.entrySet().toArray()));
+        // or: duplicatesV2.forEach( (k, v) -> System.out.print(k + "="+ v + ", "));
+
+        System.out.println("\n--------------------------------------\n");
+        System.out.println("Input text: \n" + TEXT_CP + "\n");
+        System.out.println("\n\nIncluding Unicode surrogate pairs examples:\n");
+        System.out.println("HashMap based solution:");
+        long startTimeV3 = System.nanoTime();
+
+        Map<String, Integer> duplicatesV3 = countDuplicateCharactersVCP1(TEXT_CP);
+
+        displayExecutionTime(System.nanoTime()-startTimeV3);
+        System.out.println(Arrays.toString(duplicatesV3.entrySet().toArray()));
+        // or: duplicatesV3.forEach( (k, v) -> System.out.print(k + "="+ v + ", "));
+
+        System.out.println();
+        System.out.println("Java 8, functional-style solution:");
+        long startTimeV4 = System.nanoTime();
+
+        Map<String, Long> duplicatesV4 = countDuplicateCharactersVCP2(TEXT_CP);
+
+        displayExecutionTime(System.nanoTime()-startTimeV4);
+        System.out.println(Arrays.toString(duplicatesV4.entrySet().toArray()));
+        // or: duplicatesV4.forEach( (k, v) -> System.out.print(k + "="+ v + ", "));
+    }
+
+    private static void displayExecutionTime(long time) {
+        System.out.println("Execution time: " + time + " ns" + " (" +
+                TimeUnit.MILLISECONDS.convert(time, TimeUnit.NANOSECONDS) + " ms)");
     }
 
     public static Map<Character, Integer> countDuplicateCharactersV1(String str) {
@@ -65,13 +123,28 @@ public class App
         return result;
     }
     /**
-     * 원래 문자열에서 String.chars ()메소드를 호출하십시오. IntStream을 반환합니다. 이 IntStream은 주어진 문자열에서 문자의 정수 표현을 포함합니다.
+     * 원래 문자열에서 String.chars ()메소드를 호출하십시오. IntStream을 반환합니다.
+     * 이 IntStream은 주어진 문자열에서 문자의 정수 표현을 포함합니다.
      * mapToObj() 메소드를 통해 IntStream을 문자 스트림으로 변환합니다 (정수 표현을 인간 친화적 문자 형식으로 변환).
      * 마지막으로 문자 (Collectors.groupingBy())를 그룹화 하고 계산합니다 (Collectors.counting()).
      * */
     public static Map<Character, Long> countDuplicateCharactersV2(String str) {
+        if (str == null || str.isBlank()) {
+            return Collections.emptyMap();
+        }
         Map<Character, Long> result = str.chars()
                 .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+        return result;
+    }
+    public static Map<String, Long> countDuplicateCharactersVCP2(String str) {
+        if (str == null || str.isBlank()) {
+            return Collections.emptyMap();
+        }
+
+        Map<String, Long> result = str.codePoints()
+                .mapToObj(c -> String.valueOf(Character.toChars(c)))
                 .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
 
         return result;
